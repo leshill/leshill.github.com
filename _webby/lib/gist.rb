788be4737767c1c57c8d5cdfbe0088b6cfa11386
Les Hill <leshill@gmail.com>
2013-02-18 14:13:47 -0800

    Gists should use https

--- _webby/lib/gist.rb
+++ _webby/lib/gist.rb
@@ -2,6 +2,6 @@ Webby::Filters.register :gist do |input|
   if ENV['NO_GIST']
     input
   else
-    input.gsub(/gist(\d+)\./) { |s| "<script src='http://gist.github.com/#{$1}.js'></script>" }
+    input.gsub(/gist(\d+)\./) { |s| "<script src='https://gist.github.com/#{$1}.js'></script>" }
   end
 end
